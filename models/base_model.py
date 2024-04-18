3c985c275e93ccb28a0bc85d6e59c0f5bfb204c7
Abrar Saad <abrarsaad3572022@gmail.com>
2024-04-18 04:16:47 +0200

    enhancement

--- models/base_model.py
+++ models/base_model.py
@@ -19,7 +19,7 @@ class BaseModel:
         """Instatntiates a new model"""
         if not kwargs:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.utcnow()
+            self.created_at = datetime.now()
             self.updated_at = self.created_at
         else:
             for key, value in kwargs.items():

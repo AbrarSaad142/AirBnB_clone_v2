cc45fab792d21dc6194b61ef292a4442d194fa81
Abrar Saad <abrarsaad3572022@gmail.com>
2024-05-04 05:36:23 +0300

    update 0-index.html

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-"""This module contains the function do_deploy
-that distributes an archive to your web servers"""
-
-
+""" Fabric script (based on the file 2-do_deploy_web_static.py) that creates
+and distributes an archive to your web servers, using the function deploy: """
 from fabric.api import *
 from datetime import datetime
 from os.path import exists
@@ -10,11 +8,8 @@ do_pack = __import__('1-pack_web_static').do_pack
 do_deploy = __import__('2-do_deploy_web_static').do_deploy
 
 
-env.hosts = ["52.72.66.73", "34.227.91.227"]
-
-
 def deploy():
-    """creates and distributes an archive to your web servers"""
+    """ creates and distributes an archive to your web servers"""
     new_archive_path = do_pack()
     if exists(new_archive_path) is False:
         return False

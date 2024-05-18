2e7bd38a1aa2af5491cc7ea80ec6fe1b9892d511
Abrar Saad <abrarsaad3572022@gmail.com>
2024-05-18 16:20:34 +0300

    4. Is it a number?

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""starts a Flask web application"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def home():
+    """
+    display “Hello HBNB!”
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """
+    display "HBNB"
+    """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """
+    display display “C ” followed by the value of the text variable
+    """
+    text = text.replace('_', ' ')
+    return "C {}".format(text)
+
+
+@app.route('/python/')
+@app.route("/python/<text>", strict_slashes=False)
+def python(text='is cool'):
+    """
+    display “python” followed by the value of the text variable
+    """
+    text = text.replace('_', ' ')
+    return "Python {}".format(text)
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    """
+    display “number” followed by the value of the text variable
+    """
+    n = str(n)
+    return "{} is a number".format(n)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5000)

4a5bccaa876032e7ff42b73c1201af4e36b5e36b
Abrar Saad <abrarsaad3572022@gmail.com>
2024-05-19 01:53:20 +0300

    6. Odd or even?

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -59,5 +59,13 @@ def html_number(n):
     return render_template('5-number.html', n=n)
 
 
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def number_odd_or_even(n):
+    """
+    display “number” followed by the value of the text variable
+    """
+    return render_template('6-number_odd_or_even.html', n=n)
+
+
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
